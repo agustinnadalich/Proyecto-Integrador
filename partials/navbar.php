11ed4e0f7d65c843e34c922d9701c238237b9a18
gastee <gastee@gmail.com>
2019-05-23 20:54:43 -0300

    creacion logout y diferencia en nav entre logeado y no logeado

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,49 +1,58 @@
 
-    <div class="navbar_rec">
+  <div class="navbar_rec">
 
-    </div>
+  </div>
 
-    
-    <nav class="navbar navbar-expand-md sticky-top" role="navitation">
 
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapse_target">
-        <span class="navbar-toggler-icon"><img src="img/hambg.png" height="30" width="30"></span>
-      </button>
+<nav class="navbar navbar-expand-md sticky-top" role="navitation">
 
-                        <!-- Look Empresa -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapse_target">
+      <span class="navbar-toggler-icon"><img src="img/hambg.png" height="30" width="30"></span>
+    </button>
 
-                        <div class="collapse navbar-collapse" id="collapse_target">
-                          <a class="navbar-brand" href="index.php"><img class="logo_JM" src="img/logo_JM.png" height="50" width="50"></a>
-                          <a class="navbar-text"href="profile.php">
-                            <?php if (isset($_SESSION["userLoged"])) {
-                          echo $_SESSION["userLoged"]['username'];}?></a>
+                      <!-- Look Empresa -->
 
-                        <!-- links navbar -->
+    <div class="collapse navbar-collapse" id="collapse_target">
+      <a class="navbar-brand" href="index.php"><img class="logo_JM" src="img/logo_JM.png" height="50" width="50"></a>
 
-       <ul class="navbar-nav">
-         <li class="nav-item dropdown">
-           <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" data-target="dropdown_target">Menu
-            <span class="caret"></span>
-           </a>
-          <div class="dropdown-menu" aria-labelledby="dropdown_target">
-            <a href="index.php" class="dropdown-item">Nosotros</a>
-            <div class="dropdown-divider"></div>
-            <a href="contacto.php" class="dropdown-item">Contacto</a>
-            <a href="faq.php" class="dropdown-item">FAQ</a>
-          </div>
+                      <!-- links navbar -->
 
-         </li>
+     <ul class="navbar-nav">
+       <?php if ( !isLogged() ) : ?>
          <li class="nav-item">
            <a href="login.php" class="nav-link">Ingresar</a>
          </li>
          <li class="nav-item">
            <a href="signup.php" class="nav-link">Registrarse</a>
          </li>
-         <li class="nav-item">
-           <a href="catalogo.php" class="nav-link">Catálogo</a>
-         </li>
        </ul>
-      </div>
 
+     <?php else: ?>
+       <li class="nav-item dropdown">
+         <a class="nav-link dropdown-toggle" href="profile.php" id="dropNavBar" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+           <?php if (isset($_SESSION["userLoged"])) {
+             echo $_SESSION["userLoged"]['username'];}?>
+           </a>
+           <div class="dropdown-menu" aria-labelledby="dropNavBar">
+             <a class="dropdown-item" href="logout.php">Salir</a>
+             <a class="dropdown-item" href="profile.php">Mi perfil</a>
+           </div>
+         </li>
+       <?php endif ?>
+       
+       <li class="nav-item dropdown">
+         <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" data-target="dropdown_target">Menu
+          <span class="caret"></span>
+         </a>
 
-     </nav>
+        <div class="dropdown-menu" aria-labelledby="dropdown_target">
+          <a href="index.php" class="dropdown-item">Nosotros</a>
+          <div class="dropdown-divider"></div>
+          <a href="contacto.php" class="dropdown-item">Contacto</a>
+          <a href="faq.php" class="dropdown-item">FAQ</a>
+          <a href="catalogo.php" class="dropdown-item">Catálogo</a>
+        </div>
+       </li>
+
+    </div>
+ </nav>
